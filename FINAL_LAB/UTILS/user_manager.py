649c67ed792a345ae74a67dd28397adcc002a25c
danielbvillanueva <157968317+danielbvillanueva@users.noreply.github.com>
2024-05-07 09:18:50 +0800

    Add files via upload

---
+++ FINAL_LAB/UTILS/user_manager.py
@@ -0,0 +1,22 @@
+class UserManager:
+
+def __init__():
+    pass
+
+def load_users():
+    pass
+
+def save_users():
+    pass
+
+def validate_user_name():
+    pass
+
+def validate_user_password():
+    pass
+
+def register(): 
+    pass
+
+def log_in():
+    pass   
